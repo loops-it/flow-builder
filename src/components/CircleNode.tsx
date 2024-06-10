3038621c20052476345e1f52cef1b46d3d73a5d7
tj <thinupavani@gmail.com>
2024-06-10 12:49:14 +0530

    circle update

--- src/components/CircleNode.tsx
+++ src/components/CircleNode.tsx
@@ -8,8 +8,8 @@ export default memo((id: any ) => {
     <>
 
       <div className="wrappers" style={{ overflow: 'visible' }}>
-        <div className="inner" style={{ color: '#333 !important', overflow: 'visible' }}>
-          <p style={{ color: '#333 !important' }}>
+        <div className="inner" style={{ color: '#383D44 !important', overflow: 'visible' }}>
+          <p style={{ color: '#383D44 !important' }}>
             Start
           </p>
         </div>
--- src/components/EndCircleNode.tsx
+++ src/components/EndCircleNode.tsx
@@ -16,13 +16,13 @@ export default memo((id: any) => {
     <>
       <div className="wrapper"  style={{ overflow: 'visible' }}>
         {/* <div className="inner">{label || 'no node connected'}</div> */}
-        <div className="inner" style={{color: '#333 !important',  overflow: 'visible' }}>
+        <div className="inner" style={{color: '#383D44 !important',  overflow: 'visible' }}>
         <div style={{ display: 'flex', justifyContent: 'end', position: 'absolute', top: -15, right: -5 }}>
             <button className='nodeCloseButton' onClick={deleteNode}>
               <RiCloseCircleFill style={{ color: '#000 !important', fontSize: '20px !important' }} />
             </button>
           </div>
-          <p style={{color: '#333 !important'}}>
+          <p style={{color: '#383D44 !important'}}>
           End
           </p>
         </div>
